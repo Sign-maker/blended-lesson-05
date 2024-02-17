9b1586c648b44b0e016c5b5c6be9e3da1ecbf431
Sign-maker <muratovdv76@gmail.com>
2024-02-17 18:55:59 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { useEffect } from 'react';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/rates',
+        element: <Rates />,
+      },
+      {
+        path: '*',
+        element: <Navigate to="/" />,
+      },
+    ],
+  },
+]);
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const success = pos => {
+      const crd = pos.coords;
+      //dispatch()
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    };
+
+    const error = err => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    });
+  }, []);
+
+  return <RouterProvider router={router} />;
 };
